b63c8695e3d12e66318f924faa0c57eb972100c2
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2023-06-22 12:47:04 -0400

    Initial Commit

---
+++ Shotbot/ShotbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShotbotApp.swift
+//  Shotbot
+//
+//  Created by Richard Witherspoon on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ShotbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

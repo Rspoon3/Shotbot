ca2101a09eca0dc81026bf1350b67c0986760542
Ricky Witherspoon <42879920+Rspoon3@users.noreply.github.com>
2025-06-28 07:29:39 -0400

    fixes

--- ShotbotCore/Sources/HomeFeature/HomeView/HomeViewV2.swift
+++ ShotbotCore/Sources/HomeFeature/HomeView/HomeViewV2.swift
@@ -189,16 +189,20 @@ public struct HomeViewV2: View {
         isExporting = true
         defer { isExporting = false }
         
-        let exportView = FramedScreenshotsComposition(screenshots: selectedImages)
+        let exportView = FramedScreenshotsComposition(
+            screenshots: selectedImages,
+            spacing: spacing,
+            padding: padding
+        )
         let renderer = ImageRenderer(content: exportView)
-        renderer.scale = displayScale
+//        renderer.scale = displayScale
         
         if let image = renderer.uiImage {
             exportedImage = image
             showShareSheet = true
         }
         
-//        1902 × 2620 frame but a 3932 x 5368 export
+//        1902 × 2620 frame but a 3804 x 5240 export
     }
 }
 
